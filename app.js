b174bb68c73225988306f8d1cda35a930c4f6c09
idearjunPKM <thurthree.d@gmail.com>
2015-10-30 23:37:42 +0700

    2015/10/30

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 angular.module('app', [])
-  .controller('AppController', function () {
+  .controller('AppController', function ($http) {
     var scope = this
-    scope.name = 'CoFen'
+
+    scope.master = {
+      // timestamp: {Date.getTime()},
+      iot_id: '7',
+      temperature: 38,
+      relative_humidity: 60
+    }
+
+    // addData()
+
+    // scope.submit = function (input) {
+    //   saveData(input)
+    // }
+
+    // function addData () {
+    //   $http.get('/route/api')
+    //     .then(function success (response) {
+    //       scope.iotdata = response.data
+    //     }, function error (response) {
+    //       alert(response.data.message)
+    //     })
+    // }
+
+  // function saveData (data) {
+  //   $http.post('/route/api', data)
+  //     .then(function success (response) {
+  //       console.log(response)
+  //       addData()
+  //       alert('Success')
+  //     }, function error (response) {
+  //       alert(response.data.message)
+  //     })
+  // }
   })
--- models/data.js
+++ models/data.js
@@ -2,7 +2,7 @@ var restful = require('node-restful')
 var mongoose = restful.mongoose
 
 var data = new mongoose.Schema({
-  timestamp: String,
+  timestamp: { type: Date, default: Date.now },
   iot_id: String,
   temperature: Number,
   relative_humidity: Number
