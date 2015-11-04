6db4f9bae358af7c15b56143f105445f2675f25a
idearjunPKM <thurthree.d@gmail.com>
2015-11-04 23:03:47 +0700

    change code + del dir

--- route/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-var Data = require('../models/data')
-
-Data.methods(['get', 'put', 'post', 'delete'])
-Data.register(router, '/data')
-
-module.exports = router
